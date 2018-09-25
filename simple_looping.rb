4ccbf9490718e44d5e583b9fed78820659908a16
Sydney Grant <sg3957@nyu.edu>
2018-09-25 18:18:43 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,7 +9,8 @@
 def loop_iterator(number_of_times)
  counter = 0
   loop do
-    puts "Welcome to Flatiron School's Web Development Course!"
+    phrase =  "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     counter +=1
     if counter == number_of_times
       break
